ffecf9d486ca4c824bfdff60f39c8ba6e82a35aa
Alex Freer <chasealex6@hotmail.com>
2021-12-30 17:33:56 -0500

    made changes

--- COSC3P98_FinalProject/Header.h
+++ COSC3P98_FinalProject/Header.h
@@ -6,3 +6,18 @@
 #include <FreeImage.h>
 #include <glm/glm.hpp>
 using namespace glm;
+
+struct Material {
+	vec3 amb;
+	vec3 diff;
+	vec3 spec;
+	float shine;
+};
+
+struct glob {
+	float screenSizeX, screenSizeY;
+};
+
+glob global = {
+	500, 500 //screenX, screenY
+};
