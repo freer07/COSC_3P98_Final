75cddd59bfcc8f54627a7135d6f32eed373f7b48
Alex Freer <chasealex6@hotmail.com>
2021-12-30 23:04:06 -0500

    added objects for setup

--- COSC3P98_FinalProject/Header.h
+++ COSC3P98_FinalProject/Header.h
@@ -5,19 +5,151 @@
 #include <freeglut.h>
 #include <FreeImage.h>
 #include <glm/glm.hpp>
+#include <vector>
 using namespace glm;
+using namespace std;
 
 struct Material {
 	vec3 amb;
 	vec3 diff;
 	vec3 spec;
 	float shine;
+	Material(vec3 _amb, vec3 _diff, vec3 _spec, float _shine) {
+		amb = _amb;
+		diff = _diff;
+		spec = _spec;
+		shine = _shine;
+	}
+};
+
+class Object {
+protected:
+	Material* material;
+	vec3 centre;
+public:
+
+};
+
+class Sphere : public Object {
+	float radius;
+public:
+	Sphere(const vec3 ctr, Material* mat, float rad) {
+		radius = rad;
+		centre = ctr;		
+		material = mat;
+	}
+};
+
+class Cube : public Object {
+	float length;
+public:
+	Cube(const vec3 ctr, Material* mat, float len) {
+		length = len;
+		centre = ctr;		
+		material = mat;
+	}
+};
+
+class Plane : public Object {
+	float length;
+	float width;
+public:
+	Plane(const vec3 ctr, Material* mat, float len, float wdt) {
+		length = len;
+		width = wdt;
+		centre = ctr;
+		material = mat;
+	}
+};
+
+class Triangle : public Object {
+	float side;
+public:
+	Triangle(const vec3 ctr, Material* mat, float sd) {
+		side = sd;
+		centre = ctr;
+		material = mat;
+	}
+};
+
+class Cylinder : public Object {
+	float radius;
+	float height;
+public: 
+	Cylinder(const vec3 ctr, Material* mat, float rad, float hgt) {
+		radius = rad;
+		centre = ctr;
+		material = mat;
+		height = hgt;
+	}
+};
+
+class Camera {
+	vec3 camLoc, centre, upVector;
+	float camFov;
+public:
+	Camera() {
+
+	}
+	Camera(vec3 eL, vec3 ctr, vec3 upVctr, float fov) {
+		camLoc = eL;
+		centre = ctr;
+		upVector = upVctr;
+		camFov = fov;
+	}
+};
+
+class Light {
+	vec3 location, direction, colorRGB;
+public:
+	Light(vec3 dir, vec3 rgb, vec3 loc) {
+		direction = dir;
+		colorRGB = rgb;
+		location = loc;
+	}
+};
+
+class Ray {
+	vec3 location, direction;
+public :
+	Ray(vec3 l, vec3 d) {
+		location = l;
+		direction = d;
+	}
+};
+
+class Hit {
+	vec3 location, norm;
+	Material* mat;
+public:
+
 };
 
 struct glob {
-	float screenSizeX, screenSizeY;
+	float screenSizeX, screenSizeY, depth;
+	vec3 ambientLight;
+	vector<Object*> objects;
+	vector<Light*> lights;
+	Camera camera;
+	glob(float sX, float sY, float dpth, vector<Object*>* objs, vector<Light*>* lgts, Camera* cam) {
+		screenSizeX = sX;
+		screenSizeY = sY;
+		depth = dpth;
+		objects = *objs;
+		lights = *lgts;
+		camera = *cam;
+		ambientLight = vec3(0.5, 0.5, 0.5);
+	}
+
+	glob(float sX, float sY, float dpth, vector<Object*>* objs, vector<Light*>* lgts, Camera* cam, vec3 ambLght) {
+		screenSizeX = sX;
+		screenSizeY = sY;
+		depth = dpth;
+		objects = *objs;
+		lights = *lgts;
+		camera = *cam;
+		ambientLight = ambLght;
+	}
 };
 
-glob global = {
-	500, 500 //screenX, screenY
-};
+glob* global = new glob(500.0, 500.0, 100.0, new vector<Object*>, new vector<Light*>, new Camera, vec3(0.2, 0.2, 0.2));
--- COSC3P98_FinalProject/Source.cpp
+++ COSC3P98_FinalProject/Source.cpp
@@ -8,21 +8,45 @@
 #include <FreeImage.h>
 #include "Header.h"
 #include <glm/glm.hpp>
+#include <vector>s
 using namespace glm;
+using namespace std;
+
+void imageSetup() {
+	//TODO: read in text doc to setup image 
+	//For now hard code to test
+
+	Camera* cam = new Camera(vec3(global->screenSizeX / 2, global->screenSizeY / 2, 50), 
+							 vec3(global->screenSizeX / 2, global->screenSizeY / 2, 0), 
+							 vec3(0, 1, 0),
+							 45.0);
+	global->camera = *cam;
+
+	global->lights.push_back(new Light(vec3(1, 1, 1), vec3(1, 1, 1), vec3(global->screenSizeX / 2, global->screenSizeY / 2, 0)));
+
+	global->objects.push_back(new Sphere(vec3(global->screenSizeX, 0, global->depth / 2),
+							  new Material(vec3(0.1, 0.1, 0.1), vec3(0.0, 0.37, 0.17), vec3(0.5, 0.5, 0.5), 0.3), 
+							  20));
+
+}
+
 
 int main(int argc, char** argv) {
+	imageSetup();
 	
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 
-	glutInitWindowSize(global.screenSizeX, global.screenSizeY);
-	glutCreateWindow("SIMPLE DISPLAY");
+	glutInitWindowSize(global->screenSizeX, global->screenSizeY);
+	glutCreateWindow("Ray Tracer");
 	glShadeModel(GL_SMOOTH);
 	/*glutDisplayFunc(display_image);
 	glutKeyboardFunc(keyboard);*/
 	glMatrixMode(GL_PROJECTION);
-	glOrtho(0, global.screenSizeX, 0, global.screenSizeY, 0, 1);
+	glOrtho(0, global->screenSizeX, 0, global->screenSizeY, 0, global->depth);
 
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glEnable(GL_DEPTH_TEST);
 	/*init_menu();
 	show_keys();*/
 
